07de5adab8e11b3163beee73fb8b9010d65f6ce6
Sakotik <a.shishanovskaya@gmail.com>
2023-07-16 20:55:44 +0300

    10t

--- src/main/java/ru/yandex/practicum/filmorate/storage/user_storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user_storage/UserStorage.java
@@ -2,12 +2,15 @@ package ru.yandex.practicum.filmorate.storage.user_storage;
 
 import ru.yandex.practicum.filmorate.model.User;
 
+
 import java.util.Map;
+
 
 public interface UserStorage {
 
     User createUser(User user);
 
     User updateUser(User user);
+
     Map<Integer, User> getUsers();
 }
