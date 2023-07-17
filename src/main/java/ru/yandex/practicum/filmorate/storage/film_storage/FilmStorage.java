e45db8a851461dd909c42bee86e705255bf4ed32
Sakotik <a.shishanovskaya@gmail.com>
2023-07-17 20:38:07 +0300

    10t

--- src/main/java/ru/yandex/practicum/filmorate/storage/film_storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film_storage/FilmStorage.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.storage.film_storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.util.Map;
 
@@ -10,4 +12,8 @@ public interface FilmStorage {
     Film updateFilm(Film film);
 
     Map<Integer, Film> getFilms();
+
+    Map<Integer, Genre> getGenres();
+
+    Map<Integer, Mpa> getMpa();
 }
